cbd1963e379e79c4be3b6b4f55d09fbbdf2b9ef9
faraz.usman <farazusman5@gmail.com>
2025-02-10 12:40:32 +0500

    sidebar collapse working

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -4,12 +4,17 @@ import { Component } from '@angular/core';
   selector: 'app-sidebar',
   standalone: false,
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
   isCollapsed = false;
+  activeDropdown: string | null = null;
 
   toggleSidebar(): void {
     this.isCollapsed = !this.isCollapsed;
+  }
+
+  toggleSubMenu(menu: string): void {
+    this.activeDropdown = this.activeDropdown === menu ? null : menu;
   }
 }
